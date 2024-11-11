d1a7dc5872fe47cd932916db2155308390b8b9fb
samurg <samueligcorp@gmail.com>
2024-11-11 13:45:46 +0100

    Initial commit

---
+++ src/main/java/org/example/Conexion.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.sql.*;
+
+public class Conexion {
+    String url = "jdbc:mysql://localhost:3306/ejercicio_paises";
+    String user = "root";
+    String password = "root";
+    Connection conexion;
+    Statement st;
+
+    public Conexion() {
+
+        try{
+            this.conexion = DriverManager.getConnection(url, user, password);
+            st = conexion.createStatement();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public ResultSet consulta(String query) {
+        ResultSet respuesta;
+        try{
+            respuesta = st.executeQuery(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return respuesta;
+    }
+
+    public int update(String query) {
+        int respuesta;
+        try {
+            respuesta= st.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return respuesta;
+    }
+}
